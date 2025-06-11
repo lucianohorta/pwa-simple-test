a390f427971e9def3fcd5a4686f27d272bb45b36
Luciano Horta <lucianohortasantos@gmail.com>
2025-06-11 00:52:49 -0300

    fix(): fix storageBucket name

--- notifier/index.js
+++ notifier/index.js
@@ -18,7 +18,7 @@ admin.initializeApp({
     apiKey: "AIzaSyBk_c4KxIWghSZYWiTesJP4Ho9XXdp4XWs",
     authDomain: "pwa-ios-bba82.firebaseapp.com",
     projectId: "pwa-ios-bba82",
-    storageBucket: "pwa-ios-bba82.appspot.com",
+    storageBucket: "pwa-ios-bba82.firebasestorage.app",
     messagingSenderId: "894142973830",
     appId: "1:894142973830:web:2f124ebbd5e183b7b58e07",
 });
